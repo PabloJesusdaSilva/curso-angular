8b8aaa4fb4890339e44eef285761c34a5c69304c
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-08-17 16:31:27 -0300

    Criando função para pular para próxima página

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CursoService } from './curso.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-cursos',
@@ -9,10 +10,25 @@ import { CursoService } from './curso.service';
 export class CursosComponent implements OnInit {
   
   cursos: any[] = [];
+  pagina: number = 0;
 
-  constructor(private cursoService: CursoService) {}
+  constructor(
+    private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.cursos = this.cursoService.getCursos();
+
+    this.route.queryParams.subscribe((queryParams: any) => {
+      this.pagina = queryParams['pagina'];
+    })
+  }
+
+  proxPagina() {
+    this.router.navigate(['/cursos'],
+      {queryParams: { 'Pagina': ++this.pagina}}
+    );
   }
 }
